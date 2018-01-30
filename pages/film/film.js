b5b191717c677b6a3d807501cf99b5f2afbc8ddc
Wendyltanpcy <youlinai233@gmail.com>
2018-01-30 13:35:19 +0800

    添加附近影院功能

--- pages/film/film.js
+++ pages/film/film.js
@@ -1,17 +1,28 @@
 //using zan ui
-var Zan = require('../../dist/index.js');
+var Zan = require('../../dist/index.js')
 var apiKey  = '631599923d56215aaca2a9c039b040ce'
+var sliderWidth = 96 // 需要设置slider的宽度，用于计算中间位置
 
-Page(Object.assign({}, Zan.NoticeBar, {
+Page(Object.assign({}, Zan.NoticeBar,Zan.card ,{
   data: {
+
+    //noticebar
     movable: {
       text: '====================================这块区域还在施工中!======================================='
     },
 
+    //tabbar
+    tabs: ["影片查询", "最新影片", "周边影城"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0,
+
+    //searchbar
      inputShowed: false,
      inputVal: "",
      if_serach:false,
 
+     //search_film_info
      filmInfo:{
        title: "",
        act:"",
@@ -29,8 +40,21 @@ Page(Object.assign({}, Zan.NoticeBar, {
        tag:"",
        //年份
        year:""
-     }
+     },
+     //new film
+     movies:[],
+     //theater
+     area:[],
+     theater:[],
+
+     current_movie :0
   },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    },
    showInput: function () {
         this.setData({
             inputShowed: true
@@ -57,6 +81,10 @@ Page(Object.assign({}, Zan.NoticeBar, {
     handleSearch:function(){
       //使用搜索框中的名字进行搜索
       var that = this
+      //先把上次的置空
+      that.setData({
+        filmInfo : {}
+      })
        wx.request({
           url: 'https://op.juhe.cn/onebox/movie/video',
           header: {
@@ -95,14 +123,91 @@ Page(Object.assign({}, Zan.NoticeBar, {
       })
 
     },
+    //影片图片改变时
+    imageChange:function(e){
+      this.setData({
+        current_movie : e.detail.current
+      })
+    },
   onLoad:function(options){
+    var that = this
     wx.setNavigationBarTitle({
-      title: '影片查询'
+      title: '电影专区'
+    })
+
+    wx.getSystemInfo({
+        success: function(res) {
+            that.setData({
+                sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+            });
+        }
+    });
+
+    //处理最新的电影信息，发出请求
+    wx.request({
+      url :"https://m.maoyan.com/movie/list.json",
+      data:{
+        type:"hot",
+        offset:"0",
+        limit:"1000"
+      },
+       header: {
+        'content-type': 'application/json' // 默认值
+      },
+      dataType : 'json',
+      success: function(res) {
+        that.setData({
+          movies : res.data.data.movies
+        })
+        console.log(that.data.movies)
+      },
+      fail: function(res) {
+        console.log(res.data)
+      }    
+  
+    })
+
+    //处理附近的影院信息
+    wx.request({
+      url :"https://m.maoyan.com/cinemas.json",
+       header: {
+        'content-type': 'application/json' // 默认值
+      },
+      dataType : 'json',
+      success: function(res) {
+        that.setData({
+           area : Object.entries(res.data.data)
+        })
+        var tempList=[]
+        var tempvar = 1
+        for(var i=0;i<that.data.area.length;i++){
+          for(var j =0;j<that.data.area[i][tempvar].length;j++){
+            tempList.push(that.data.area[i][tempvar][j])
+          }
+        }
+    
+        that.setData({
+          theater : tempList
+        })
+        console.log(that.data.theater)
+      },
+      fail: function(res) {
+        console.log(res.data)
+      }    
+  
     })
   
   },
   onShow:function() {
     // 滚动通告栏需要initScroll
     this.initZanNoticeBarScroll('movable');
+  },
+   onUnload:function(){
+    // 页面关闭
+    //把电影信息置空
+    this.setData({
+      filmInfo : {}
+    })
   }
 }))
